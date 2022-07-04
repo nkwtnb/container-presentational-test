b1058ea239a9adcded7eb57e99b9a449a8cc048e
nkwtnb <n.watanabe0305@gmail.com>
2022-07-04 22:35:16 +0900

    不要コード削除、; 追加など

--- src/components/list/tests/Presenter.test.tsx
+++ src/components/list/tests/Presenter.test.tsx
@@ -12,28 +12,29 @@ describe("Presenterのテスト", () => {
   test("todos = 0件 の時、「データなし」が表示されること", async () => {
     await act(() => {
     render(<Presenter todos={[]} />);
-    })
+    });
     expect(screen.getByTestId("no-todos")).toHaveTextContent("データなし");
-  })
+  });
   test("todos > 0件 の時、todo表示要素「todos」が表示されること", async () => {
     await act(() => {
       render(<Presenter todos={[{id: 1, title: "test", userId: 1, completed: true}]} />);
-    })
+    });
     // state更新後の再描画を待機
     await waitFor(() => {
       expect(screen.getByTestId("todos").children.length).toBe(1);
-    })
+    });
   });
-
   test("スナップショットが一致していること", async () => {
     let element: RenderResult;
     await act(() => {
       element = render(<ListContainer />);
-    })
+    });
+    // 初期値は[]の為、0件の表示
     expect(screen.getByTestId("no-todos")).toHaveTextContent("データなし");
+    // 200件取得されるまで待機
     await waitFor(() => {
       expect(screen.getByTestId("todos").children.length).toBe(200);
-    })
+    });
     // asFragment > toMatchSnapshotでスナップショットテスト
     expect(element!.asFragment()).toMatchSnapshot();
   });
