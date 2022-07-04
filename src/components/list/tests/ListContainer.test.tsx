fb11fbd41ac6c13df1468c370446ab2bfb79777d
nkwtnb <n.watanabe0305@gmail.com>
2022-07-04 12:13:34 +0900

    不要コード削除

--- src/components/list/tests/ListContainer.test.tsx
+++ src/components/list/tests/ListContainer.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, prettyDOM, render, renderHook, RenderResult, screen, waitFor } from '@testing-library/react';
+import { cleanup, render, waitFor } from '@testing-library/react';
 import ListContainer from "../ListContainer";
 import * as Presenter from '../Presenter';
 import { act } from 'react-dom/test-utils';
@@ -11,9 +11,8 @@ afterEach(() => {
 describe("ListContainerのテスト", () => {
   test("Containerから todos の引数が渡されていること。", async () => {
     const mock = jest.spyOn(Presenter, "default");
-    let element: RenderResult;
     await act(() => {
-      element = render(<ListContainer />);
+      render(<ListContainer />);
     });
     await waitFor(() => {
       expect(mock).toBeCalledTimes(2);
